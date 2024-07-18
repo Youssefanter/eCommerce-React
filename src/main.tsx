bfa7003aea01991b80f6c25ecd5a5a05b2bcd0cd
Youssefanter <yooiialy@gmail.com>
2024-07-18 14:13:44 +0300

    Create the main layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom/client";
+import MainLayout from "./layouts/MainLayout/MainLayout";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<div>Hello</div>);
+ReactDOM.createRoot(document.getElementById("root")!).render(<MainLayout />);
