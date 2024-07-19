2ca6db4efe96fd91e4e3c64f6cea69c91e0393e7
Youssefanter <yooiialy@gmail.com>
2024-07-19 19:45:58 +0300

    Finished first video

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,13 +1,23 @@
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 const Error = () => {
+  const error = useRouteError();
+  let errorStatus: number;
+  let errorStatusText: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorStatus = error.status;
+    errorStatusText = error.statusText;
+  } else {
+    errorStatus = 404;
+    errorStatusText = "Not Found";
+  }
   return (
     <Container className="notFound">
-      <h1>404</h1>
-      <p>Not Found</p>
+      <h1>{errorStatus}</h1>
+      <p>{errorStatusText}</p>
       <Link to={"/"} replace={true}>
-        Look's like you've reached to non-existent page. <br />
         Hou about going back to safety?
       </Link>
     </Container>
